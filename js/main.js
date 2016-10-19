5475d1c2d01560aa8ac43b02268f35be23922c16
NatashaDianeey <ndianeey@gamil.com>
2016-10-19 12:49:52 +0000

    Bicis Locas, segunda parte

--- js/main.js
+++ js/main.js
@@ -1,35 +1,57 @@
 function validateForm(){
-	/* Escribe tú código aquí */
-	var nombre = document.getElementById('name').value;
+
+	
+	var spans = document.getElementsByClassName('error');
+    while(spans.length>0 ) {
+    	spans[0].parentElement.removeChild(spans[0])
+    };
+
+    var nombre = document.getElementById('name').value;
 	var apellido = document.getElementById('lastname').value;
 	var contrasena = document.getElementById('input-password').value;
 	var correo = document.getElementById('input-email').value;
-	var seleccionar = document.querySelector("#selectoption").value;
 
-	if(nombre === "" || apellido === "" || correo === "" || contrasena === "") {
-		document.getElementById('errorError').style.background ='#ccffcc';
-		alert('Falta llenar algun campo');
-		return false;
+			//NOMBRE
+        if (nombre === "" || nombre == null || !/^[A-Z][a-z]*$/g.test(document.getElementById('name').value) ) {
+            var mensajeValidoCon = document.createElement("span");
+            mensajeValidoCon.setAttribute ("class", "error");
+            mensajeValidoCon.innerHTML= "Ingrese Nombre."
+			var boxBlack = document.getElementsByClassName("input-box")[0].appendChild(mensajeValidoCon);
+			
 
-	} else if (!/^[A-Z][a-z]*$/g.test(document.getElementById('name').value) ){
-		alert('Unicamente la primera letra debe ser Mayuscula');
-		return false;
+			//APELLIDO
+		} if (apellido === "" || apellido == null || !/^[A-Z][a-z]*$/g.test(document.getElementById('lastname').value) ){
+			var mensajeValidoCon = document.createElement("span");
+			mensajeValidoCon.setAttribute ("class", "error");
+        	mensajeValidoCon.innerHTML= "Ingrese Apellido."
+			var boxBlack = document.getElementsByClassName("input-box")[1].appendChild(mensajeValidoCon);
+        }  
+        	//CONTRASEÑA
+        if (contrasena.length < 5 || contrasena == "123456" || contrasena =="password" || contrasena == "098754"){
+        	var mensajeValidoCon = document.createElement("span");
+        	mensajeValidoCon.setAttribute ("class", "error");
+        	mensajeValidoCon.innerHTML= "Ingrese contraseña."
+			var boxBlack = document.getElementsByClassName("input-box")[3].appendChild(mensajeValidoCon);
 
-	} else if (!/^[A-Z][a-z]*$/g.test(document.getElementById('lastname').value) ){
-		alert('Unicamente la primera letra debe ser Mayuscula');
-		return false;
+        } //CORREO
+        var expr = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        if (!expr.test(correo) ){
+			if(correo == ""){
+				var mensajeValidoCon = document.createElement("span");
+				mensajeValidoCon.setAttribute ("class", "error");
+				mensajeValidoCon.innerHTML= "Ingrese su correo."
+				var boxBlack = document.getElementsByClassName("input-box")[2].appendChild(mensajeValidoCon);
+	       }
+	    }	//SELECCIONAR OPCIONES DE BICI
+   		var seleccionar = document.querySelector("#selectoption").value;
+        if ( seleccionar == 0){
+            var mensajeValidoCon = document.createElement("span");
+            mensajeValidoCon.setAttribute ("class", "error");
+			mensajeValidoCon.innerHTML= "Selecciona una opción."
+			var boxBlack = document.getElementsByClassName("input-box")[4].appendChild(mensajeValidoCon);
+        }
+    }
 
-	}else if (contrasena.length<6) {
-		alert('La contrasena es muy corta');
-		return false;
+///////////Para quitar los spans despues de hacer submit al boton Registrar
 
-	} else if (contrasena === 'contraseña' || contrasena == 123456 || contrasena == 0987654) {
-		alert('Esta contraseña no es permitida');
-		return false;
-	
-	} else if (seleccionar == 0) {
-		alert("Elige tu tipo de bici.");
-
-	}
-	
-}
+    
